71e452b1086c98e8cd56901dfafb44d06e32c549
Florian Grässle <git@holehan.org>
2018-07-23 13:16:30 +0200

    hugo: Update from 0.44 to 0.45

--- packages/hugo/build.sh
+++ packages/hugo/build.sh
@@ -1,8 +1,8 @@
 TERMUX_PKG_HOMEPAGE=https://gohugo.io/
 TERMUX_PKG_DESCRIPTION="Configurable static site generator"
 TERMUX_PKG_MAINTAINER="Florian Grässle <termux@holehan.org>"
-TERMUX_PKG_VERSION=0.44
-TERMUX_PKG_SHA256=63e6ae4be7e7b23ab5551842182b49be9a9a07a82d217c1a7a2815b49976129d
+TERMUX_PKG_VERSION=0.45
+TERMUX_PKG_SHA256=3c7798efe488fcbf3471f608c9e08e5087cf686b6c8237a625e659e0386010e1
 TERMUX_PKG_SRCURL=https://github.com/gohugoio/hugo/archive/v${TERMUX_PKG_VERSION}.tar.gz
 
 termux_step_make_install(){
@@ -28,7 +28,7 @@ termux_step_make_install(){
 
   termux_download https://github.com/gohugoio/hugo/releases/download/v${TERMUX_PKG_VERSION}/hugo_extended_${TERMUX_PKG_VERSION}_Linux-64bit.tar.gz \
     $TERMUX_PKG_CACHEDIR/hugo_extended_${TERMUX_PKG_VERSION}_Linux-64bit.tar.gz \
-    9ebed7fba3e12ce104dbdf525e52b8ed956c3d361ef3989b40ef484c82d43a6e
+    8ed6ed41d2e3a5921853e5c8625271b0412fb2581a19d4c0f287b19f305dbd98
   tar xf $TERMUX_PKG_CACHEDIR/hugo_extended_${TERMUX_PKG_VERSION}_Linux-64bit.tar.gz -C $GOPATH/bin/ hugo
 
   mkdir -p $TERMUX_PREFIX/{etc/bash_completion.d,share/man/man1}
